547feabc08260ca7a4e06d5a201d1db5c1736127
Austin Hacker <explosivepyrounicorns@gmail.com>
2019-10-02 15:34:20 -0400

    Use api key

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 async function getRandomWallpaper(apiToken) {
-	const results = await axios.get('https://cors-anywhere.herokuapp.com/https://wallhaven.cc/api/v1/search')
+	let url = 'https://cors-anywhere.herokuapp.com/https://wallhaven.cc/api/v1/search?sorting=random&resolutions=1920x1080'
+
+	if (apiToken !== null && apiToken.length > 0){
+		url += `&apikey=${apiToken}`
+	}
+
+	const results = await axios.get(url)
 	return results.data.data[0].path
+}
+
+async function getUserSettings(apiToken) {
+	const results = await axios.get(`https://cors-anywhere.herokuapp.com/https://wallhaven.cc/api/v1/settings?apikey=${apiToken}`)
+	return results.data.data
 }
 
 const init = _ => {
@@ -8,7 +19,10 @@ const init = _ => {
     	.then( async ({ apiToken }) => {
 			const wallPath = await getRandomWallpaper(apiToken);
 			document.body.style.backgroundImage = `url('${wallPath}')`;
-		} );
+		}).catch( async () => {
+			const wallPath = await getRandomWallpaper(null);
+			document.body.style.backgroundImage = `url('${wallPath}')`;
+		});
 };
 
 init();
